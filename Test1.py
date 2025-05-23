b78afde9908ff355d1fbeea1e387ea8654d29756
koladik <koladik@gmail.com>
2025-05-23 18:36:15 +0300

    update graphs

--- Test1.py
+++ Test1.py
@@ -6,16 +6,55 @@ from qmath import QBinom, QPochhammer
 
 
 def main():
-    q = 0.5
-    c = get_qc(10, 2, q)
+    q = 0.8
+    t = 0
+    n_max = 30
+    t = np.array([5., 25, 125, 625])
+    lt = np.array([5, 10, 25,])
+    n = np.arange(0, n_max)
+    #%%
+    c_q = [get_qc(n_max, _t, q) for _t in t]
+    #sigma_q = [np.sqrt(np.sum((n**2)*(_c - np.sum(n*_c)))) for _c in c_q]
+    c = [get_c(n_max, 0.5*_t) for _t in lt]
+    mu = [np.sum(n * _c) for _c in c]
+    sigma = [np.sum((n**2)*(_c - np.sum(n*_c))) for _c in c]
+
     fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.plot(c)
+    ax = [fig.add_subplot(2,2,i+1) for i in range(4)]
+    ax[0].set_xlabel("n")
+    ax[1].set_xlabel("n")
+    for _c in c:
+        _c = np.where(_c >= 0.0001, _c, np.nan)
+        _c = np.log(_c) + n*np.log(2)
+        ax[0].plot(n, _c, "-o", color="black", markersize=3)
+    #ax[0].set_ylim([np.log(0.001), 1.1])
+    for _c in c_q:
+        _c = np.where(_c >= 0.0001, _c, np.nan)
+        _c = np.log(_c) + n*np.log(2)
+        ax[1].plot(_c, "-o", color="black", markersize=3)
+    #ax[1].set_ylim([-80, 1.1])
+
+    ax[2].plot(lt, mu)
     plt.show()
 
+
 def get_c(n_max, t=1):
+    """
+    Вычисляет c_n(pt) = (2pt)^n / n! * e^(-pt) для n от 0 до n_max-1.
+    """
+    t = mp.mpf(t)
+    # Вычисляем общий множитель e^(-pt)
+    exp_t = mp.exp(-t)
+    # Инициализируем список результатов
     c = np.zeros(n_max)
+    # Вычисляем c_n для каждого n
+    for n in range(n_max):
+        numerator = mp.power(t, n)  # (2pt)^n
+        factorial_n = mp.factorial(n)  # n!
+        c[n] = (numerator / factorial_n) * exp_t
+
     return c
+
 
 def get_qc(n_max, t = 1, q = 0.5):
     r"""
