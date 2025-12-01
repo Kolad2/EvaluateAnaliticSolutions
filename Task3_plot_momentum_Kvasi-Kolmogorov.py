206868b2dbc6b678e6856ab9fdbe10a00daa5499
Kolad2 <koladik@gmail.com>
2025-12-01 05:11:17 +0300

    update solution base

--- Task3_plot_momentum_Kvasi-Kolmogorov.py
+++ Task3_plot_momentum_Kvasi-Kolmogorov.py
@@ -14,7 +14,7 @@ def main():
 
     #%%
     t = 10
-    q = 0.91
+    q = 0.92
 
     eta_2, eta_3 = evaluate_moments(t, q)
     print(eta_2, eta_3)
@@ -23,15 +23,21 @@ def main():
 def evaluate_moments(t=1, q=0.9):
     n_max = 60
     n = np.arange(0, n_max)
-    c_q = c_tr_pow(n_max, t, q)
-    n_0 = np.sum(c_q)
-    m_1 = np.sum(n * c_q / n_0)
-    eta_2 = np.sqrt(np.sum((n - m_1) ** 2 * c_q / n_0))
-    eta_3 = np.cbrt(np.sum((n - m_1) ** 3 * c_q / n_0))
+    c = c_tr_pow(n_max, t, q)
+    
+    if not np.all(c > 0):
+        Warning("Wrong Solution, c < 0")
+    
+    n_0 = np.sum(c)
+    pdf = c / n_0
+    c = None
+    m_1 = np.sum(n * pdf)
+    eta_2 = np.sqrt(np.sum((n - m_1) ** 2 * pdf))
+    eta_3 = np.cbrt(np.sum((n - m_1) ** 3 * pdf))
 
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(n,c_q)
+    ax.plot(n, pdf)
     plt.show()
 
     return eta_2, eta_3
